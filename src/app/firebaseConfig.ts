3420b5ef0c7c4dd2ef0c6b46fddb8ef7024b93e6
David Edet <davidedet47@gmail.com>
2020-05-26 20:01:51 +0100

    updated-with-resume-v1.1

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -1,10 +1,1 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-    apiKey: "AIzaSyA6pCt7COphMzX9qy6m5i5mSNZiQ3j27VE",
-    authDomain: "mysite-3a248.firebaseapp.com",
-    databaseURL: "https://mysite-3a248.firebaseio.com",
-    projectId: "mysite-3a248",
-    storageBucket: "mysite-3a248.appspot.com",
-    messagingSenderId: "1087407430097",
-    appId: "1:1087407430097:web:0d38f8a3bc30b927fee8f0"
-};
+// 
