c5a38c4ae0b84db8e8fe713fbdc42f30b235e08c
David Edet <davidedet47@gmail.com>
2020-07-26 11:05:34 +0100

    new-message-26-07-2020

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./../../assets/css/split.css']
+  styleUrls: ['./../../assets/css/split.css', './home.component.css']
 })
 export class HomeComponent implements OnInit {
 
